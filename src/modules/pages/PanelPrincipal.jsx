a6f6a408042fb6f4295f2e42997267440c14be58
JhosepSF <jhosepsanflo@gmail.com>
2024-10-04 11:06:33 -0500

    Fin

--- src/modules/pages/PanelPrincipal.jsx
+++ src/modules/pages/PanelPrincipal.jsx
@@ -31,6 +31,22 @@ export function PanelPrincipal() {
           <img src={`../img/imagen3.jpg`} alt="Imagen 3" />
           <p className="legend">Imagen 3</p>
         </div>
+        <div>
+          <img src={`../img/imagen4.jpg`} alt="Imagen 4" />
+          <p className="legend">Imagen 4</p>
+        </div>
+        <div>
+          <img src={`../img/imagen5.jpg`} alt="Imagen 5" />
+          <p className="legend">Imagen 5</p>
+        </div>
+        <div>
+          <img src={`../img/imagen6.jpg`} alt="Imagen 6" />
+          <p className="legend">Imagen 6</p>
+        </div>
+        <div>
+          <img src={`../img/imagen7.jpg`} alt="Imagen 7" />
+          <p className="legend">Imagen 7</p>
+        </div>
         </Carousel>
       </div>
     </div>
